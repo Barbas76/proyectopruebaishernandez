3b47c5dea03e189656ed022a3204f745648d7443
Barbas76 <164250471+Barbas76@users.noreply.github.com>
2024-08-03 10:49:03 -0600

    Hernandez OK
    Cambio 1

---
+++ ProyectoPruebaisHernandez/src/proyectopruebaishernandez/ProyectoPruebaisHernandez.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyectopruebaishernandez;
+
+/**
+ *
+ * @author Jorge Hernandez
+ */
+public class ProyectoPruebaisHernandez {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Prueba GitHub Hernandez OK....");
+    }
+    
+}
